ff70b20b706cbd8500150381d7b6c904048c5a31
Martin O. Pollard <mp15@sanger.ac.uk>
2015-01-13 13:51:13 +0000

    More useful unsorted index error message
    Make error message when file positions are out of order more helpful by stating which positions are out of order

--- hts.c
+++ hts.c
@@ -846,7 +846,7 @@ int hts_idx_push(hts_idx_t *idx, int tid, int beg, int end, uint64_t offset, int
         idx->z.last_tid = tid;
         idx->z.last_bin = 0xffffffffu;
     } else if (tid >= 0 && idx->z.last_coor > beg) { // test if positions are out of order
-        if (hts_verbose >= 1) fprintf(stderr, "[E::%s] unsorted positions\n", __func__);
+        if (hts_verbose >= 1) fprintf(stderr, "[E::%s] unsorted positions %d: %d > %d\n", __func__, tid, idx->z.last_coor, beg);
         return -1;
     }
     if ( tid>=0 )
